f51ced3f7a205e47c3ef3d3949310490a391273e
Alan Tran <contactalantran@gmail.com>
2020-08-30 20:07:16 -0400

    Finish parallax landing page design

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const parallax = document.getElementById("parallax");
+const parallax2 = document.getElementById("div3");
+const parallax3 = document.getElementById("div5");
 
 window.addEventListener("scroll", ()=>{
     let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY = offset * 0.9 + 'px';
+    parallax.style.backgroundPositionY = `${offset * 0.01}em`;
     // the .6 determines the speed. If bigger than one, it'll scroll the other way.
+    parallax2.style.backgroundPositionY = `${offset * -0.055}em`;
+    parallax3.style.backgroundPositionY = `${offset * -0.42}px`;
 })
