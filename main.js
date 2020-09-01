68bc61059d64d8da22914c1efdb72ab8282635b9
Alan Tran <contactalantran@gmail.com>
2020-09-01 00:44:48 -0400

    improve design and tweak parallax

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 const parallax = document.getElementById("parallax");
-const parallax2 = document.getElementById("div3");
-const parallax3 = document.getElementById("div5");
 
 window.addEventListener("scroll", ()=>{
-    let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY = `${offset * 0.01}em`;
+    let offset = window.scrollY;
+    parallax.style.backgroundPositionY = `${offset * 0.7}px`;
     // the .6 determines the speed. If bigger than one, it'll scroll the other way.
-    parallax2.style.backgroundPositionY = `${offset * -0.055}em`;
-    parallax3.style.backgroundPositionY = `${offset * -0.42}px`;
+
 })
