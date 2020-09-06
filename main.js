56128966e5c516df6fd80d816aa281105fd21f36
Alan Tran <contactalantran@gmail.com>
2020-09-06 00:06:47 -0400

    Make homepage mobile responsive

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const parallax = document.getElementById("parallax");
+const checkpoint = 200;
 
-window.addEventListener("scroll", ()=>{
+window.addEventListener("scroll", () => {
     let offset = window.scrollY;
-    parallax.style.backgroundPositionY = `${offset * 0.7}px`;
+    parallax.style.backgroundPositionY = `${offset * 0.6}px`;
     // the .6 determines the speed. If bigger than one, it'll scroll the other way.
+    
 
+    //Code below is for "scroll down" prompt
+    if (offset <= checkpoint) {
+      opacity = 1 - offset / checkpoint;
+    } else {
+      opacity = 0;
+    }
+    document.querySelector("#h4").style.opacity = opacity;
 })
