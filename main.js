eb1dfdad3282c3cffd4a5b772ab63a22c07e88be
Alan Tran <contactalantran@gmail.com>
2020-09-16 00:45:00 -0400

    simplify project to one page

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-const parallax = document.getElementById("parallaxPic");
-const parallax2 = document.getElementById("secondParallax");
-const target = document.getElementsByClassName('scroll');
-const checkpoint = 200;
- 
-window.addEventListener("scroll", () => {
-  let offset = window.scrollY;
-  parallax.style.backgroundPositionY = `${offset * 0.4}px`;
-  // the .6 determines the speed. If bigger than one, it'll scroll the other way.
-  
-  for(let i = 0; i < target.length; i++) {
+const parallax = document.getElementById("parallaxPic"),
+      parallax2 = document.getElementById("secondParallax"),
+      target = document.getElementsByClassName('scroll'),
+      checkpoint = 200,
+      options = {};
 
+function setScroll() {
+  let offset = window.scrollY;
+  parallax.style.backgroundPositionY = `${offset * 0.7}px`;
+  // the .6 determines the speed. If bigger than one, it'll scroll the other way.
+
+  //Code below is for "scroll down" prompt
+  if (offset <= checkpoint) {
+    opacity = 1 - offset / checkpoint;
+  } else {
+    opacity = 0;
+  }
+  document.querySelector("h5").style.opacity = opacity;
+  
+  //make parallax effect for site header
+  for(let i = 0; i < target.length; i++) {
     var posX = window.pageYOffset * target[i].dataset.ratex;
     var posY = window.pageYOffset * target[i].dataset.ratey;
     target[i].style.transform = 'translate3d('+posX+'px, '+posY+'px, 0px)';
-
-    //Code below is for "scroll down" prompt
-    if (offset <= checkpoint) {
-      opacity = 1 - offset / checkpoint;
-    } else {
-      opacity = 0;
-    }
-    document.querySelector("h5").style.opacity = opacity;
   }
-});
+
+}
 
 function removeCSSfade(){
   for(let i = 0; i < target.length; i++) {
@@ -31,4 +33,20 @@ function removeCSSfade(){
   }
 };
 
-setTimeout(removeCSSfade, 3000);
+setTimeout(removeCSSfade, 2100);
+
+
+
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        window.addEventListener("scroll", setScroll);
+      } else {
+        window.removeEventListener("scroll", setScroll);
+      }
+      console.log(entry);
+    })
+}, options);
+
+
+observer.observe(parallax);
