b28e2e1eccab51da27758be95fad565d401a4eee
Alan Tran <contactalantran@gmail.com>
2020-10-03 18:50:00 -0400

    Refactor background image algorithm

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const sections    = document.querySelectorAll(".section"),
+const sections    = document.querySelectorAll(".breakpoints"),
       parallax    = document.getElementById("parallaxPic"),
       target      = document.getElementsByClassName('scroll'),
       checkpoint  = 200,
-      images      = document.querySelectorAll(".background"),
+      image      = document.querySelector(".background"),
       navbar      = document.querySelector(".navbar"),
       options     = {
                     rootMargin: "0px 0px -95%"
@@ -38,12 +38,38 @@ function removeCSSfade(){
 
 setTimeout(removeCSSfade, 2100);
 
+const backgroundArray = [
+  "./img/new_york/newyorktop1.jpg",
+  "./img/new_york/newyorktop2.jpg",
+  "./img/new_york/newyorktop3.jpg",
+  "./img/new_york/newyorktop4.jpg",
+  "./img/new_york/newyorktop5.jpg",
+  "./img/new_york/newyorktop6.jpg",
+  "./img/new_york/newyorktop7.jpg",
+  "./img/new_york/newyorktop8.jpg",
+]
+
+function randomImage() {
+  image.src = backgroundArray[Math.floor(Math.random() * backgroundArray.length)];
+};
+
+function fadeOut() {
+  image.style.opacity = 0;
+}
+
+function fadeIn() {
+    image.style.opacity = 1;
+}
+
+function workYoMagic() {
+  fadeOut();
+  setTimeout(randomImage, 900);
+  setTimeout(fadeIn, 1200);
+}
+
 const observePic = new IntersectionObserver((entry, observePic) => {
   if (!entry.isIntersecting) {
-      images.forEach(image => {
-        image.classList.remove("visible");
-      });
-      images[Math.floor(Math.random() * 8)].classList.add("visible");
+    workYoMagic();
   };
 }, options)
 
